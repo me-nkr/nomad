678126293cec4e3bf758a69fc74c74c8cb7220ec
Naveen K R <thenaveenkr7@gmail.com>
2023-08-08 22:43:49 +0530

    add some basic config options

---
+++ core/options.lua
@@ -0,0 +1,18 @@
+local opt = vim.opt -- aliasing
+
+-- line number
+opt.number = true
+
+-- tabs & intendation
+opt.tabstop = 4
+opt.shiftwidth = 4
+opt.expandtab = true
+opt.autoindent = true
+
+-- search
+opt.ignorecase = true
+opt.smartcase = true
+
+-- appearance
+opt.termguicolors = false
+opt.background = "dark"
