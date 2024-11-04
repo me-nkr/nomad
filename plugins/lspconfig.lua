dac220aef2503ee182fffbc2cd78b9e0be636554
menkr <thenaveenkr7@gmail.com>
2024-11-04 15:38:11 +0530

    remove typescript lsp

--- plugins/lspconfig.lua
+++ plugins/lspconfig.lua
@@ -3,6 +3,3 @@ local status, lspconfig = pcall(require, "lspconfig")
 if not status then
   return print("nvim-lspconfig not installed")
 end
-
--- configure typescript-laguage-server
-lspconfig.tsserver.setup{}
